58dbabb9049ac5130ba8b9122e778c7a6a1ad00f
Chad Wilken <chad.wilken@gmail.com>
2015-05-29 16:16:33 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email  "me@chadwilken.com"
 description       "Monit configs for server components"
 version           "0.1.2"
 
+recipe "monit_configs-ctw::postgres", "Monit config for Postgres"
 recipe "monit_configs-ctw::nginx", "Monit config for nginx"
 recipe "monit_configs-ctw::redis-server", "Monit config for redis server"
 
